4dfbef9e99be054df7e2089bcdb307dfb4dae9da
Carl Nysten <nysten.carl@gmail.com>
2022-03-11 14:45:57 +0200

    parses lines and makes 2d int arr

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <cnysten@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 14:21:23 by cnysten           #+#    #+#             */
-/*   Updated: 2022/03/10 10:33:33 by carlnysten       ###   ########.fr       */
+/*   Updated: 2022/03/11 14:39:54 by carlnysten       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,10 +46,27 @@ t_list	*read_lines(int fd)
 	return (lines);
 }
 
-int	**init_points(int n_rows, int n_cols)
+// Allocates and initialises
+static int	**int_array_2d(int n_rows, int n_cols)
 {
+	int		**points;
+	int		*cells;
+	int		i;
+	size_t	size;
 
-
+	size = n_rows * sizeof (int *) + n_cols * sizeof (int);
+	points = (int **) malloc(size);
+	if (!points)
+		die("Malloc fail.");
+	ft_bzero(points, size);
+	cells = (int *)(points + n_rows);
+	i = 0;
+	while (i < n_rows)
+	{
+		points[i] = cells + i * n_cols;
+		i++;
+	}
+	return (points);
 }
 
 int	**parse_lines(t_list *lines)
@@ -59,7 +76,7 @@ int	**parse_lines(t_list *lines)
 	int		n_cols;
 	size_t	n_rows;
 	int		i = 0;
-	//int		j = 0;
+	int		j = 0;
 	int		k = 0;
 
 	split = ft_strsplit(lines->content, ' ');
@@ -69,11 +86,23 @@ int	**parse_lines(t_list *lines)
 		n_cols++;
 	printf("number of columns %d\n", n_cols);
 	printf("number of rows %zu\n", n_rows);
-	points = (int **) malloc(n_rows * sizeof (int *) + n_cols * sizeof (int));
-	if (!points)
-		die("Malloc failed");
+	points = int_array_2d(n_rows, n_cols);
 	while (split[i])
 		points[0][k++] = ft_atoi(split[i++]);
+	free(split);
+	lines = lines->next;
+	j = 1;
+	while (lines)
+	{
+		i = 0;
+		k = 0;
+		split = ft_strsplit(lines->content, ' ');
+		while (split[i])
+			points[j][k++] = ft_atoi(split[i++]);
+		free(split);
+		j++;
+		lines = lines->next;
+	}
 	return (points);
 }
 
@@ -93,12 +122,14 @@ int	**points_from_file(char *filename, t_vars *vars)
 	t_list	*lines;
 	int		**points;
 
+	(void) vars;
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		die(USAGE);
 	lines = read_lines(fd);
 	print_lines(lines);
 	points = parse_lines(lines);
+	//free lines
 	return (points);
 }
 
